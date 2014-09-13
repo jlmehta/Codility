675f5e9645905958d1b2276724f42a8b8bbd5783
jlmehta <jlmehta10@gmail.com>
2014-09-13 14:49:12 -0400

    Minimun Missing Integer

---
+++ Main/MinMissingInteger/MinMissingIntegerProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.MinMissingInteger
+{
+    /// <summary>
+    /// Store the given Array in a HashSet, then iterates from 1 to figure 
+    /// out the smallest one not present in the set.
+    /// Time Complexity: O(N), Theta(2N) actually
+    /// Space Complexity: O(N)
+    /// Codility Score 100%
+    /// Result Link: https://codility.com/demo/results/demoBYH3JZ-6V4/
+    /// 
+    /// </summary>
+    public class MinMissingIntegerProblem
+    {
+        public int solution(int[] a)
+        {
+            if (a == null || a.Length == 0)
+                return 0;
+
+            HashSet<int> set = new HashSet<int>();
+
+            foreach(int i in a)
+            {
+                set.Add(i);
+            }
+
+            for (int i = 1; i <= int.MaxValue; i++)
+                if(!set.Contains(i))
+                return i;
+
+            return int.MaxValue;
+        }
+    }
+}
---
+++ Test/MinMissingIntegerTest.cs
@@ -0,0 +1,57 @@
+﻿using Main.MinMissingInteger;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class MinMissingIntegerTest
+    {
+        private MinMissingIntegerProblem msp;
+
+        [SetUp]
+        public void Init()
+        {
+            msp = new MinMissingIntegerProblem();
+        }
+
+        [Test]
+        public void MinMissingIntegerTest1()
+        {
+            Assert.AreEqual(msp.solution(new int[] { 1, 2, 3, 5 }), 4);
+        }
+
+        [Test]
+        public void MissingIntegerTestZero()
+        {
+            Assert.AreEqual(msp.solution(new int[0]), 0);
+        }
+
+        [Test]
+        public void MissingIntegerTestNull()
+        {
+            Assert.AreEqual(msp.solution(null), 0);
+        }
+
+        [Test]
+        public void MissingIntegerTestOne()
+        {
+            Assert.AreEqual(msp.solution(new int[] {2}), 1);
+        }
+
+        [Test]
+        public void MissingIntegerTestTwo()
+        {
+            Assert.AreEqual(msp.solution(new int[] { 1 }), 2);
+        }
+
+        [Test]
+        public void MissingIntegerTestNegative()
+        {
+            Assert.AreEqual(msp.solution(new int[] { -1, 4, -5, 1 }), 2);
+        }
+    }
+}
