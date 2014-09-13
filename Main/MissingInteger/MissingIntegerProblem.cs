4f10da29c9ce18fc80c6d94d557ab04313448b0d
jlmehta <jlmehta10@gmail.com>
2014-09-13 14:02:08 -0400

    Missing Integer Problem commit

---
+++ Main/MissingInteger/MissingIntegerProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Main.MissingInteger
+{
+    public class MissingIntegerProblem
+    {
+        /// <summary>
+        /// Correct solution with acceptable performance.
+        /// This solution computes the sum of the given number and finds the max to find the missing number.
+        /// Time Complexity: O(N) 
+        /// Space Complexity: O(1)
+        /// Codility Score 100%
+        /// Result Link: https://codility.com/demo/results/demo9P2T47-RVG/
+        /// </summary>
+        /// 
+        public int solution(int[] a)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            if (a == null) 
+                return 0;
+
+            if (a.Length < 1) 
+                return 1;
+
+            double max = 0;
+            double sum = 0;
+            foreach(int i in a)
+             {
+                 max = (max > i) ? max : i;
+                 sum += i;
+             }
+
+            if (max > a.Length + 1)
+                throw new ApplicationException("Unacceptable input");
+
+            int missingNumber = (int)((double)(max * (max + 1) / 2) - sum);
+
+            watch.Stop();
+            System.Diagnostics.Debug.WriteLine(a.Length + " elements: " + watch.ElapsedMilliseconds);
+            return missingNumber == 0 ? (int)max+1 : missingNumber;
+        }
+    }
+}
---
+++ Test/MissingIntegerTest.cs
@@ -0,0 +1,105 @@
+﻿using Main.MissingInteger;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class MissingIntegerTest
+    {
+        private MissingIntegerProblem msp;
+
+        [SetUp]
+        public void Init()
+        {
+            msp = new MissingIntegerProblem();
+        }
+
+        [Test]
+        public void MissingIntegerTest1()
+        {
+            Assert.AreEqual(msp.solution(new int[] { 1, 2, 3, 5 }), 4);
+        }
+
+        [Test]
+        public void MissingIntegerTestZero()
+        {
+            Assert.AreEqual(msp.solution(new int[0]), 1);
+        }
+
+        [Test]
+        public void MissingIntegerTestOne()
+        {
+            Assert.AreEqual(msp.solution(new int[] {1}), 2);
+        }
+
+        [Test]
+        public void MissingIntegerTestTwo()
+        {
+            Assert.AreEqual(msp.solution(new int[] { 1,2 }), 3);
+        }
+
+        [Test]
+        public void MissingIntegerTestLast()
+        {
+            Assert.AreEqual(msp.solution(new int[] {1,2,3,4}), 5);
+        }
+
+        [Test]
+        public void MissingIntegerTestFirst()
+        {
+            Assert.AreEqual(msp.solution(new int[] { 2, 3, 4 }), 1);
+        }
+
+        [Test]
+        public void MissingIntegerTestNull()
+        {
+            Assert.AreEqual(msp.solution(null), 0);
+        }
+
+        [Test]
+        public void MissingIntegerTestMedium()
+        {
+            int num = 0;
+            Assert.AreEqual(msp.solution(GetLargeArray(10000, out num)), num);
+        }
+
+        [Test]
+        public void MissingIntegerTestLarge()
+        {
+            int num = 0;
+            Assert.AreEqual(msp.solution(GetLargeArray(100000, out num)), num);
+        }
+
+        [Test]
+        public void MissingIntegerTestHuge()
+        {
+            int num = 0;
+            Assert.AreEqual(msp.solution(GetLargeArray(1000000, out num)), num);
+        }
+
+        private int[] GetLargeArray(int n, out int random)
+        {
+            var r = new Random();
+            var arr = new int[n-1];
+            var list = new List<int>();
+            for(int i =1; i<=n; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i=0; i<n-1; i++)
+            {
+                int num = r.Next(list.Count);
+                arr[i] = list[num];
+                list[num] = list.Last();
+                list.RemoveAt(list.Count-1);
+            }
+            random = list.First();
+            return arr;
+        }
+    }
+}
