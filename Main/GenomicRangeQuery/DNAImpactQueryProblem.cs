6dc6ed9cab02eabb914f80b5bf0f74ab6af2ef98
jlmehta <jlmehta10@gmail.com>
2014-09-18 00:00:48 -0400

    Initial commit Geonomic Impact Factor

---
+++ Main/GenomicRangeQuery/DNAImpactQueryProblem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.GenomicRangeQuery
+{
+    public class DNAImpactQueryProblem
+    {
+        public int[] Solution(string s, int[] p, int[] q)
+        {
+            short a, c, g;
+            int counter = 1;
+            int[,] prefixArray = new int[3, s.Length + 1];
+
+            foreach(char ch in s)
+            {
+                a = 0; c = 0; g = 0;
+                switch (ch)
+                {
+                    case 'A':
+                        a = 1;
+                        break;
+                    case 'C':
+                        c = 1;
+                        break;
+                    case 'G':
+                        g = 1;
+                        break;
+                    default: 
+                        break;
+                }
+                prefixArray[0, counter] = prefixArray[0, counter - 1] + a;
+                prefixArray[1, counter] = prefixArray[1, counter - 1] + c;
+                prefixArray[2, counter] = prefixArray[2, counter - 1] + g;
+                counter++;
+            }
+
+            return p.Zip(q, (pi, qi) => GetMinimalImpactFactor(pi, qi, prefixArray)).ToArray();
+        }
+
+        private int GetMinimalImpactFactor(int pi, int qi, int[,] prefixArray)
+        {
+            if (prefixArray[0, qi + 1] > prefixArray[0, pi])
+                return 1;
+            else if (prefixArray[1, qi + 1] > prefixArray[1, pi])
+                return 2;
+            else if (prefixArray[2, qi + 1] > prefixArray[2, pi])
+                return 3;
+            return 4;
+        }
+    }
+}
---
+++ Test/DNAImpactQueryTest.cs
@@ -0,0 +1,38 @@
+﻿using Main.GenomicRangeQuery;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class DNAImpactQueryTest
+    {
+        private DNAImpactQueryProblem s;
+
+        [SetUp]
+        public void Init()
+        {
+            s = new DNAImpactQueryProblem();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = s.Solution("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 });
+            int[] expected = new int[] {2,4,1};
+            Assert.AreEqual(result.Zip(expected, (r,e) => r==e).Any(x => x==false), false);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = s.Solution("A", new int[] { 0 }, new int[] { 0 });
+            int[] expected = new int[] { 1 };
+            Assert.AreEqual(result.Zip(expected, (r, e) => r == e).Any(x => x == false), false);
+        }
+    }
+}
